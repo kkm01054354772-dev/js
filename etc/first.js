48a7be2a7757eb46f0ff07b5971d2e830cb0d7dd
KimGyeongmin <kkm01054354772@gmail.com>
2025-11-21 10:14:21 +0900

    ajax까지

--- etc/first.js
+++ etc/first.js
@@ -1,17 +1,24 @@
 // 일급함수 : 함수를 일반적인 값(정수, 문자열...)과 동일하게 취급하는 개념
-// 변수에 할당
-// 다른 함수의 인자로 전달
-// 다른 함수의 리턴값으로 사용
 
+// 1. 변수에 할당
 const func1 = () => {
   console.log("변수 할당");
 };
 
 func1();
 
+// 2. 다른 함수의 인자로 전달
 function logText(msg) {
   msg();
 }
-
 // logText() 호출하면서 함수를 전달인자로 사용
 logText(() => console.log("hi"));
+
+// 3. 다른 함수의 리턴값으로 사용
+function add(x) {
+  return function (y) {
+    return x + y;
+  };
+}
+const sum = add(5);
+console.log(sum(6));
